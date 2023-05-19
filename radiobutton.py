aa5e87ce8743a30524ebef56088e067257bf6d5f
Wilder Duran Yanes <130265317+WDuran24@users.noreply.github.com>
2023-05-18 21:58:14 -0400

    Add files via upload

---
+++ radiobutton.py
@@ -0,0 +1,25 @@
+from tkinter import * 
+
+def seleccionar():
+    monitor.config(text='{}'.format(opcion.get()))
+
+def reset():
+    opcion.set(None)
+    monitor.config(text='')
+# Configuracion de la raiz
+
+root=Tk()
+
+opcion=IntVar()
+
+Radiobutton(root,text='Opcion 1',variable=opcion,value=1,command=seleccionar).pack()
+Radiobutton(root,text='Opcion 2',variable=opcion,value=2,command=seleccionar).pack()
+Radiobutton(root,text='Opcion 3',variable=opcion,value=3,command=seleccionar).pack()
+
+monitor=Label(root)
+monitor.pack()
+
+Button(root,text='Reiniciar',command=reset).pack()
+# Finalmente bucle de la aplicacion
+
+root.mainloop()
